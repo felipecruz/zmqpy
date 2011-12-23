3fc24d5f7d64af5df66ca6f9085a71b7c8bd1b47
Felipe cruz <felipecruz@loogica.net>
2011-12-23 19:40:37 -0200

    send frame with binary content test and fixing send zframe test

--- zmqpy/tests/test_zframe.py
+++ zmqpy/tests/test_zframe.py
@@ -19,7 +19,18 @@ class TestZFrames(BaseZMQTestCase):
         self.assertTrue(rc == 0)
 
         frame_recv = zmqpy.ZFrame.recv(s2)
-        self.assertEquals(message, "zmq message")
+        self.assertEquals(frame_recv.data, "zmq message")
+
+    def test_send_frame_binary_content(self):
+        s1, s2 = self.create_bound_pair(zmqpy.PAIR, zmqpy.PAIR)
+        message = u"zmq message"
+        frame_content = zmqpy.ZFrame(message.encode('utf-8'))
+        rc = frame_content.send(s1, 2)
+        self.assertTrue(rc == 0)
+
+        frame_recv = zmqpy.ZFrame.recv(s2)
+        self.assertEquals(frame_recv.data.decode('utf-8'), u"zmq message")
+
 
 if __name__ == "__main__":
     unittest.main()
