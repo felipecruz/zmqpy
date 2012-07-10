1e3366e0fb98fe7d16a3ed01e375bc8b0140f4fb
Felipe cruz <felipecruz@loogica.net>
2012-07-09 21:02:13 -0300

    remove trailing spaces

--- zmqpy/tests/test_loop.py
+++ zmqpy/tests/test_loop.py
@@ -10,16 +10,16 @@ class TestLoop(BaseZMQTestCase):
     def test_loop(self):
         loop = zmqpy.Loop(verbose=True)
         s1, s2 = self.create_bound_pair(zmqpy.PAIR, zmqpy.PAIR)
-        
+
         def timer_event(loop, item, args):
             zmqpy.czmq.zstr_send(args, 'message')
             return 0
 
         def socket_event(loop, item, args):
             return -1
-        
+
         loop.timer(100, 1, timer_event, s1)
-        
+
         from zmqpy import zmq_pollitem_t
         _input = zmq_pollitem_t(s2.handle, 0, zmqpy.POLLIN)
 
@@ -31,7 +31,7 @@ class TestLoop(BaseZMQTestCase):
     def test_loop_poller_end(self):
         loop = zmqpy.Loop(verbose=True)
         s1, s2 = self.create_bound_pair(zmqpy.PAIR, zmqpy.PAIR)
-        
+
         global checklist
 
         def socket_event(loop, item, args):
@@ -56,15 +56,15 @@ class TestLoop(BaseZMQTestCase):
 
             def stop(self):
                 self.loop.destroy()
-        
+
         loop_thread = LoopThread(loop)
-        
+
         loop.poller(_input, socket_event, None)
         s1.send('message 1')
         loop.poller_end(_input)
         s1.send('message 2')
         loop.poller(_input, socket_event, None)
-        
+
         loop_thread.start()
         loop_thread.join()
         loop.destroy()
