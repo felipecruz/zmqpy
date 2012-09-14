fc680329f98749938e1208d92d5465cdec87bd6c
Felipe cruz <felipecruz@loogica.net>
2012-09-13 23:44:36 -0300

    Poller implementation - pyzmq tests working!

---
+++ zmqpy/tests/test_poller.py
@@ -0,0 +1,209 @@
+import time
+import unittest
+
+import zmqpy
+from zmqpy.utils.strtypes import asbytes
+from zmqpy import Poller
+from __init__ import BaseZMQTestCase
+
+class PollerTest(BaseZMQTestCase):
+    def test_poller_init(self):
+        poller = Poller()
+
+        assert poller
+
+    def test_poller_register(self):
+        poller = Poller()
+        socket1, socket2 = self.create_bound_pair(zmqpy.PAIR, zmqpy.PAIR)
+        poller.register(socket1)
+
+        assert poller.sockets[socket1] == zmqpy.POLLIN | zmqpy.POLLOUT
+
+    def test_poller_register_no_flags(self):
+        poller = Poller()
+        socket1, socket2 = self.create_bound_pair(zmqpy.PAIR, zmqpy.PAIR)
+        poller.register(socket1)
+
+        #register with no flags unregister the socket
+        poller.register(socket1, flags=None)
+
+        assert poller.sockets == {}
+
+    def test_poller_unregister(self):
+        poller = Poller()
+        socket1, socket2 = self.create_bound_pair(zmqpy.PAIR, zmqpy.PAIR)
+        poller.register(socket1)
+
+        #register with no flags unregister the socket
+        poller.unregister(socket1)
+
+        assert poller.sockets == {}
+
+    def test_poller_modify(self):
+        poller = Poller()
+        socket1, socket2 = self.create_bound_pair(zmqpy.PAIR, zmqpy.PAIR)
+        poller.register(socket1)
+
+        #register with no flags unregister the socket
+        poller.modify(socket1, flags=zmqpy.POLLOUT)
+
+        assert poller.sockets[socket1] == zmqpy.POLLOUT
+
+def wait():
+    time.sleep(.25)
+
+class TestPoll(BaseZMQTestCase):
+
+    Poller = zmqpy.Poller
+
+    # This test is failing due to this issue:
+    # http://github.com/sustrik/zeromq2/issues#issue/26
+    def test_pair(self):
+        s1, s2 = self.create_bound_pair(zmqpy.PAIR, zmqpy.PAIR)
+
+        # Sleep to allow sockets to connect.
+        wait()
+
+        poller = self.Poller()
+        poller.register(s1, zmqpy.POLLIN|zmqpy.POLLOUT)
+        poller.register(s2, zmqpy.POLLIN|zmqpy.POLLOUT)
+        # Poll result should contain both sockets
+        socks = dict(poller.poll())
+        # Now make sure that both are send ready.
+        self.assertEquals(socks[s1], zmqpy.POLLOUT)
+        self.assertEquals(socks[s2], zmqpy.POLLOUT)
+        # Now do a send on both, wait and test for zmqpy.POLLOUT|zmqpy.POLLIN
+        s1.send(b'msg1')
+        s2.send(b'msg2')
+        wait()
+        socks = dict(poller.poll())
+        self.assertEquals(socks[s1], zmqpy.POLLOUT|zmqpy.POLLIN)
+        self.assertEquals(socks[s2], zmqpy.POLLOUT|zmqpy.POLLIN)
+        # Make sure that both are in POLLOUT after recv.
+        s1.recv()
+        s2.recv()
+        socks = dict(poller.poll())
+        self.assertEquals(socks[s1], zmqpy.POLLOUT)
+        self.assertEquals(socks[s2], zmqpy.POLLOUT)
+
+        poller.unregister(s1)
+        poller.unregister(s2)
+
+        # Wait for everything to finish.
+        wait()
+
+    def test_reqrep(self):
+        s1, s2 = self.create_bound_pair(zmqpy.REP, zmqpy.REQ)
+
+        # Sleep to allow sockets to connect.
+        wait()
+
+        poller = self.Poller()
+        poller.register(s1, zmqpy.POLLIN|zmqpy.POLLOUT)
+        poller.register(s2, zmqpy.POLLIN|zmqpy.POLLOUT)
+
+        # Make sure that s1 is in state 0 and s2 is in POLLOUT
+        socks = dict(poller.poll())
+        self.assertEquals(s1 in socks, 0)
+        self.assertEquals(socks[s2], zmqpy.POLLOUT)
+
+        # Make sure that s2 goes immediately into state 0 after send.
+        s2.send(b'msg1')
+        socks = dict(poller.poll())
+        self.assertEquals(s2 in socks, 0)
+
+        # Make sure that s1 goes into POLLIN state after a time.sleep().
+        time.sleep(0.5)
+        socks = dict(poller.poll())
+        self.assertEquals(socks[s1], zmqpy.POLLIN)
+
+        # Make sure that s1 goes into POLLOUT after recv.
+        s1.recv()
+        socks = dict(poller.poll())
+        self.assertEquals(socks[s1], zmqpy.POLLOUT)
+
+        # Make sure s1 goes into state 0 after send.
+        s1.send(b'msg2')
+        socks = dict(poller.poll())
+        self.assertEquals(s1 in socks, 0)
+
+        # Wait and then see that s2 is in POLLIN.
+        time.sleep(0.5)
+        socks = dict(poller.poll())
+        self.assertEquals(socks[s2], zmqpy.POLLIN)
+
+        # Make sure that s2 is in POLLOUT after recv.
+        s2.recv()
+        socks = dict(poller.poll())
+        self.assertEquals(socks[s2], zmqpy.POLLOUT)
+
+        poller.unregister(s1)
+        poller.unregister(s2)
+
+        # Wait for everything to finish.
+        wait()
+
+    def test_no_events(self):
+        s1, s2 = self.create_bound_pair(zmqpy.PAIR, zmqpy.PAIR)
+        poller = self.Poller()
+        poller.register(s1, zmqpy.POLLIN|zmqpy.POLLOUT)
+        poller.register(s2, 0)
+        self.assertTrue(s1 in poller.sockets)
+        self.assertFalse(s2 in poller.sockets)
+        poller.register(s1, 0)
+        self.assertFalse(s1 in poller.sockets)
+
+    def test_pubsub(self):
+        s1, s2 = self.create_bound_pair(zmqpy.PUB, zmqpy.SUB)
+        s2.setsockopt(zmqpy.SUBSCRIBE, b'')
+
+        # Sleep to allow sockets to connect.
+        wait()
+
+        poller = self.Poller()
+        poller.register(s1, zmqpy.POLLIN|zmqpy.POLLOUT)
+        poller.register(s2, zmqpy.POLLIN)
+
+        # Now make sure that both are send ready.
+        socks = dict(poller.poll())
+        self.assertEquals(socks[s1], zmqpy.POLLOUT)
+        self.assertEquals(s2 in socks, 0)
+        # Make sure that s1 stays in POLLOUT after a send.
+        s1.send(b'msg1')
+        socks = dict(poller.poll())
+        self.assertEquals(socks[s1], zmqpy.POLLOUT)
+
+        # Make sure that s2 is POLLIN after waiting.
+        wait()
+        socks = dict(poller.poll())
+        self.assertEquals(socks[s2], zmqpy.POLLIN)
+
+        # Make sure that s2 goes into 0 after recv.
+        s2.recv()
+        socks = dict(poller.poll())
+        self.assertEquals(s2 in socks, 0)
+
+        poller.unregister(s1)
+        poller.unregister(s2)
+
+        # Wait for everything to finish.
+        wait()
+    def test_timeout(self):
+        """make sure Poller.poll timeout has the right units (milliseconds)."""
+        s1, s2 = self.create_bound_pair(zmqpy.PAIR, zmqpy.PAIR)
+        poller = self.Poller()
+        poller.register(s1, zmqpy.POLLIN)
+        tic = time.time()
+        evt = poller.poll(timeout=.005)
+        toc = time.time()
+        self.assertTrue(toc-tic < 0.1)
+        tic = time.time()
+        evt = poller.poll(timeout=5)
+        toc = time.time()
+        self.assertTrue(toc-tic < 0.1)
+        self.assertTrue(toc-tic > .001)
+        tic = time.time()
+        evt = poller.poll(timeout=500)
+        toc = time.time()
+        self.assertTrue(toc-tic < 1)
+        self.assertTrue(toc-tic > 0.1)
--- zmqpy/zmqpy.py
+++ zmqpy/zmqpy.py
@@ -197,3 +197,66 @@ class Socket(object):
         C.zmq_msg_close(zmq_msg)
 
         return value
+
+def make_zmq_pollitem(socket, flags):
+    zmq_socket = socket.zmq_socket
+    zmq_pollitem = ffi.new('zmq_pollitem_t*')
+    zmq_pollitem.socket = zmq_socket
+    zmq_pollitem.fd = 0
+    zmq_pollitem.events = flags
+    zmq_pollitem.revents = 0
+    return zmq_pollitem[0]
+
+def _poll(zmq_pollitem_list, poller, timeout=-1):
+    items = ffi.new('zmq_pollitem_t[]', zmq_pollitem_list)
+    list_length = ffi.cast('int', len(zmq_pollitem_list))
+    c_timeout = ffi.cast('long', timeout * 1000)
+    C.zmq_poll(items, list_length, c_timeout)
+    result = []
+    for index in range(len(items)):
+        if items[index].revents > 0:
+            result.append((poller._sockets[items[index].socket],
+                           items[index].revents))
+    return result
+
+
+# Code From PyZMQ
+class Poller(object):
+    def __init__(self):
+        self.sockets = {}
+        self._sockets = {}
+        self.c_sockets = {}
+
+    def register(self, socket, flags=POLLIN|POLLOUT):
+        if flags:
+            self.sockets[socket] = flags
+            self._sockets[socket.zmq_socket] = socket
+            self.c_sockets[socket] =  make_zmq_pollitem(socket, flags)
+        elif socket in self.sockets:
+            # uregister sockets registered with no events
+            self.unregister(socket)
+        else:
+            # ignore new sockets with no events
+            pass
+
+    def modify(self, socket, flags=POLLIN|POLLOUT):
+        self.register(socket, flags)
+
+    def unregister(self, socket):
+        del self.sockets[socket]
+        del self._sockets[socket.zmq_socket]
+        del self.c_sockets[socket]
+
+    def poll(self, timeout=None):
+        if timeout is None:
+            timeout = -1
+
+        timeout = int(timeout)
+        if timeout < 0:
+            timeout = -1
+
+        items =  _poll(self.c_sockets.values(),
+                       self,
+                       timeout=timeout)
+
+        return items
