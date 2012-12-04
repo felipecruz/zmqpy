35a397c04690db63cf49bb3d10c278aca17e8b1b
Felipe cruz <felipecruz@loogica.net>
2012-12-04 14:28:20 -0200

    minor setup change

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
-'''
-Created on May 20, 2010
-
-@author: felipe
-'''
-
 from distutils.core import setup
+import zmqpy._cffi
 
 setup(name='zmqpy',
-      version='0.1a',
-      description='Python czmq ctypes bindings',
+      version='0.2',
+      description='Python cffi-based ZeroMQ bindings',
       author='Felipe Cruz',
       author_email='felipecruz@loogica.net',
-      url='http://test.com',
-     packages=['zmqpy', 'zmqpy/tests/', 'zmqpy/utils/'],
-     )
+      url='http://loogica.net/opensource/',
+      install_requires=['cffi >=0.3'],
+      packages=['zmqpy', 'zmqpy/tests/', 'zmqpy/utils/'])
